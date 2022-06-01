091219fcc914e8276ad5ed09e6511266deace5b4
testfortest25865 <97685031+testfortest25865@users.noreply.github.com>
2022-06-01 22:32:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".slider_smm").slick();
+  $(".slider_sayt").slick();
+  $(".slider_seo").slick();
+  $(".slider_port").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
